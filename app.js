e17f0b6375f2af431d6f3d5dfc6be5f3c94dc67c
saileshgg <saileshgurung36@gmail.com>
2021-12-30 19:27:41 +0530

    dothtraki template

--- app.js
+++ app.js
@@ -2,7 +2,32 @@ var buttonHandler = document.querySelector("#button");
 var textHandler = document.querySelector("#inputText");
 var outputHandler = document.querySelector("#output");
 
+var url = 'https://api.funtranslations.com/translate/dothraki.json';
+// const encoded = encodeURI(url);
 
-buttonHandler.addEventListener("click", function() {
-    outputHandler.innerText =  textHandler.value;
-})
+function getTranslatedURL(text) {
+    return url + "?" +"text=" + text;
+}
+
+function errorHandler(error) {
+    console.log(error);
+    console.log("Server not working!!");
+}
+
+function clickHandler() {
+
+    var inputText = textHandler.value;
+    
+
+    fetch(getTranslatedURL(inputText))
+        .then(response => response.json())
+        .then(data => {
+            var translated = data.contents.translated
+            outputHandler.innerText = translated
+        })
+        .catch(errorHandler)
+
+
+}
+
+buttonHandler.addEventListener("click", clickHandler)
